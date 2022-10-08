06ed7ff1202d90e0c8acf5de76a0ae9c167a073c
Loay Mady <loay.mady13@gmail.com>
2022-10-08 18:55:22 +0200

    Updates

---
+++ precache-manifest.63a1865107fe83497dfd1e2f63c8417b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "138b65cb02ef8fb8905f",
+    "url": "/E-Commerce-React--App/static/css/main.7036fd58.chunk.css"
+  },
+  {
+    "revision": "138b65cb02ef8fb8905f",
+    "url": "/E-Commerce-React--App/static/js/main.138b65cb.chunk.js"
+  },
+  {
+    "revision": "ad39ee43e5f1e39ec27e",
+    "url": "/E-Commerce-React--App/static/js/1.ad39ee43.chunk.js"
+  },
+  {
+    "revision": "a425130f9b5e7a0953be",
+    "url": "/E-Commerce-React--App/static/css/2.e60a5547.chunk.css"
+  },
+  {
+    "revision": "a425130f9b5e7a0953be",
+    "url": "/E-Commerce-React--App/static/js/2.a425130f.chunk.js"
+  },
+  {
+    "revision": "be3d48f16066a8659eb4",
+    "url": "/E-Commerce-React--App/static/js/runtime~main.be3d48f1.js"
+  },
+  {
+    "revision": "6549601b63ce27f044009b2a20bc7adb",
+    "url": "/E-Commerce-React--App/static/media/slider1.6549601b.jpg"
+  },
+  {
+    "revision": "68621cc939ca25062ce2f887ac9fe5fe",
+    "url": "/E-Commerce-React--App/static/media/slider2.68621cc9.jpg"
+  },
+  {
+    "revision": "74f84754b734014338c3e0e5f06ae21e",
+    "url": "/E-Commerce-React--App/static/media/slider3.74f84754.jpg"
+  },
+  {
+    "revision": "730d3dcca1c38c58dbb95ebd8e485133",
+    "url": "/E-Commerce-React--App/static/media/slider4.730d3dcc.jpg"
+  },
+  {
+    "revision": "b653541b8eee5b7b79476700c30161ec",
+    "url": "/E-Commerce-React--App/static/media/about1.b653541b.jpg"
+  },
+  {
+    "revision": "db61a3d9dc0035cbeb06f4060580abe8",
+    "url": "/E-Commerce-React--App/static/media/about2.db61a3d9.jpg"
+  },
+  {
+    "revision": "e8a02ef02e9f0851869bfe687c55c4b3",
+    "url": "/E-Commerce-React--App/static/media/contactUs.e8a02ef0.jpg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/E-Commerce-React--App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/E-Commerce-React--App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/E-Commerce-React--App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/E-Commerce-React--App/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/E-Commerce-React--App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "25cbd3f521064d66e3bc46779a143943",
+    "url": "/E-Commerce-React--App/index.html"
+  }
+];
